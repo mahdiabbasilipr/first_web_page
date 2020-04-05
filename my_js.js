f04612a5db5c850e5f0a8c8b1fda77cc05f11b57
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-04-05 19:16:24 +0430

    work by js and insert onclik function and load page

--- my_js.js
+++ my_js.js
@@ -1,7 +1,21 @@
-const a = 15;
-console.log("salam");
+// const a = 15;
+// console.log("salam");
 
 
-function change_text() {
-  document.getElementById("footer").innerHTML = "footer from javascript"
+// function change_text() {
+//   document.getElementById("footer").innerHTML = "footer from javascript"
+// }
+var is_hide = 0;
+function hide_about() {
+  if (is_hide == 0){
+    document.getElementById("about").style.visibility = "hidden"
+    is_hide = 1
+  }else {
+    document.getElementById("about").style.visibility = "visible"
+    is_hide = 0
+  }
+
 }
+$(function () {
+  $("#loading_page").hide();
+})
