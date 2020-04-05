a5b4e0fc252ca1f20a1207c6094992cc7a823f60
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-04-06 00:42:53 +0430

    working by jquery and fadeout the load page

--- my_js.js
+++ my_js.js
@@ -5,17 +5,24 @@
 // function change_text() {
 //   document.getElementById("footer").innerHTML = "footer from javascript"
 // }
-var is_hide = 0;
-function hide_about() {
-  if (is_hide == 0){
-    document.getElementById("about").style.visibility = "hidden"
-    is_hide = 1
-  }else {
-    document.getElementById("about").style.visibility = "visible"
-    is_hide = 0
-  }
-
-}
+// var is_hide = 0;
+// function hide_about() {
+//   if (is_hide == 0){
+//     document.getElementById("about").style.visibility = "hidden"
+//     is_hide = 1
+//   }else {
+//     document.getElementById("about").style.visibility = "visible"
+//     is_hide = 0
+//   }
+//
+// }
 $(function () {
-  $("#loading_page").hide();
+  $("#loading_page").fadeTo(1000 , 0.5).delay(2000).fadeTo(1000,0)
+
+
+  $("#navbar_about").click(
+    function(){
+      $(this).css({"color" : "red","background-color":"red"});
+    }
+  )
 })
