c0ff37fd312d87c34ce921653becf16e264fd7de
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-04-08 14:32:46 +0430

    add about modal and menu in navbar and add footer to the slideup

--- my_js.js
+++ my_js.js
@@ -16,21 +16,48 @@
 //   }
 //
 // }
-$(function () {
-  $("#loading_page").fadeTo(1000, 0.5).delay(2000).fadeTo(1000, 0).hide(1003)
+$("#modal_contact").hide()
+$("#modal_about").hide()
+$("#modal_list").hide()
 
+$(function () {
+  $("#loading_page").fadeTo(1000, 0.5).delay(2000).fadeTo(1000, 0).hide()
+  $("#wrapper").slideDown(0)
+  $("#footer").slideDown(0)
   $("#jump_down").click(
     function () {
       $("#wrapper").slideToggle(1000)
+      $("#footer").slideToggle(1000)
     }
   )
+  //
+  // $("#navbar_about").click(
+  //   function () {
+  //     $(this).css({"color": "red", "background-color": "red"});
+  //   }
+  // )
 
+
+  $("#navbar_contact").click(
+    function () {
+      $("#modal_contact").fadeTo(2000,1)
+    }
+  )
   $("#navbar_about").click(
     function () {
-      $(this).css({"color": "red", "background-color": "red"});
+      $("#modal_about").fadeTo(2000,1)
     }
   )
-
+  $(".close_modal").click(
+    function () {
+      $(".modal_wrapper").hide(1000)
+    }
+  )
+  $("#navbar_list").click(
+    function () {
+      $("#modal_list").slideToggle(1000)
+    }
+  )
   $("a").on('click',function (event) {
     if (this.hash !== ""){
       event.preventDefault();
